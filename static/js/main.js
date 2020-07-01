b3388a24ca9eb4882ce6b5fa6f54015e6b173247
David Niño Herrán <45274593+davidnherran@users.noreply.github.com>
2020-06-30 20:41:29 -0500

    Configuración inicial + renderizado de mapa

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWRuaGVycmFuIiwiYSI6ImNrYzJvYmd4MjE5OTgzOHA5dmhxaXEwcjIifQ.Q20rbBpBFxHPRGiLfbeTVA';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [-74.1483976,4.5891058],
+zoom: 15
+});
